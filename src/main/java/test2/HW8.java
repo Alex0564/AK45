a8fb14ad91eac51cd69a6e6c62277f5fe7aebf61
Academic202124 <academic198405@gmail.com>
2021-04-27 16:04:02 -0400

    Trening test2

---
+++ src/main/java/test2/HW8.java
@@ -0,0 +1,17 @@
+package test2;
+
+public class HW8 {
+    public static void main(String[] args) {
+        String a="Pagero";
+        String [] b={"Pagero","Nissan"};
+        int c=4;
+        double c1=2.5;
+
+        char e='t';
+        System.out.println(a+" "+(b[0])+" "+c+" "+c1);
+
+
+
+    }
+
+}
