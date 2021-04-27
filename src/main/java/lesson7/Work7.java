9e3df633949f876144c34c876c9de5ed188d5b93
Anachicu92 <anachicu89@gmail.com>
2021-04-27 17:28:32 -0400

    Homework done

--- src/main/java/lesson7/Work7.java
+++ src/main/java/lesson7/Work7.java
@@ -2,6 +2,16 @@ package lesson7;
 
 public class Work7 {
     public static void main(String[] args) {
-        System.out.println("Homework done");
+        System.out.println("Hello, My name is Ana");
+        System.out.println("Hello, My name is Jack");
+        System.out.println("How can i help you?");
+        System.out.println("I would like buy flight tickets");
+        System.out.println("Sure, I can help you with that");
+
+        int x=5+10;
+        System.out.println(x);
+
+
+
     }
 }
