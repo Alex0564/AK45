be9fda0841eaf00d2f4514761eca3b442a6b7732
dinat2121 <dinat2121@gmail.com>
2021-04-26 22:48:05 -0400

    congratulation

---
+++ src/main/java/DFhomework7/DFwork7.java
@@ -0,0 +1,7 @@
+package DFhomework7;
+
+public class DFwork7 {
+    public static void main(String[] args) {
+        System.out.println( "Congratulation, Dina, it's your first homework in Github!!! ");
+    }
+}
