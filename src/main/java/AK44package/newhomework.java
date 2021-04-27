fd64f60a6beacb958625e418d8924cc082d4cfb9
oleks <oleksiy564@gmail.com>
2021-04-27 15:00:33 -0400

    homework April 27 2021

---
+++ src/main/java/AK44package/newhomework.java
@@ -0,0 +1,7 @@
+package AK44package;
+
+public class newhomework {
+    public static void main(String[] args){
+        System.out.println("The homework is done!");
+    }
+}
--- src/main/java/WorkAK44.java
+++ src/main/java/WorkAK44.java
@@ -1,2 +1,5 @@
 public class WorkAK44 {
+    public static void main(String[] args){
+
+    }
 }
