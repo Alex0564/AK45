def7b497771ace478586fcac1a4e0d945b9821ae
Academic202124 <academic198405@gmail.com>
2021-04-27 16:33:57 -0400

    Trening test Build 10.1

--- src/main/java/test2/HW8.java
+++ src/main/java/test2/HW8.java
@@ -6,9 +6,10 @@ public class HW8 {
         String [] b={"Pagero","Nissan"};
         int c=4;
         double c1=2.5;
+         int [] x={4,5,6};
 
         char e='t';
-        System.out.println(a+" "+(b[0])+" "+c+" "+c1);
+        System.out.println(a+" "+(b[0])+" "+c+" "+c1+" "+(x[2]));
 
 
 
