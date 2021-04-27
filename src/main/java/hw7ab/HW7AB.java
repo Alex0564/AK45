f5008571497d9565a9a9af329d0d7e78015b5608
Val Kan <65411718+val-kan@users.noreply.github.com>
2021-04-26 23:05:18 -0400

    Update hw7ab
    This additional code was added by vk77

--- src/main/java/hw7ab/HW7AB.java
+++ src/main/java/hw7ab/HW7AB.java
@@ -3,5 +3,6 @@ package hw7ab;
 public class HW7AB {
     public static void main(String[] args) {
         System.out.println("My home work 7");
+        System.out.println("This string was added by VK77");
     }
 }
