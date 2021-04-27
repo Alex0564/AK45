3701bf48d3beb2c0f3fda0aaaf30d2b41d87f5f8
Shamik <regarit@gmail.com>
2021-04-27 15:03:45 -0400

    HomeWork 7 is finished.

--- src/main/java/hw7/Hw7.java
+++ src/main/java/hw7/Hw7.java
@@ -1,7 +1,7 @@
 package hw7;
 
-public class Work7 {
+public class Hw7 {
     public static void main(String[] args) {
-        System.out.println("Homework done!");
+        System.out.println("Hello World");
     }
 }
