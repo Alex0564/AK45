525d226bf9af3c6b95e08f28e8a94544b0ed58ea
oleks <oleksiy564@gmail.com>
2021-04-27 15:03:09 -0400

    homework April 27 2021 #2

--- src/main/java/AK44package/newhomework.java
+++ src/main/java/AK44package/newhomework.java
@@ -1,7 +1,10 @@
 package AK44package;
 
+import java.sql.SQLOutput;
+
 public class newhomework {
     public static void main(String[] args){
         System.out.println("The homework is done!");
+        System.out.println("Some change in homework");
     }
 }
