b36cd8e85dfca54fd924d8ab8ca377223be6897b
Val Kan <valeriy.kan@gmail.com>
2021-04-26 22:54:12 -0400

    Lesson7vk - created new void method myDialog().

--- src/main/java/lesson7vk/AppVK.java
+++ src/main/java/lesson7vk/AppVK.java
@@ -1,7 +1,23 @@
 package lesson7vk;
 
+import java.util.Scanner;
+
 public class AppVK {
     public static void main(String[] args) {
+        System.out.println("Hello my friend!");
+        myDialog();
+    }
+    public static void myDialog(){
+        int myAge=43;
+        System.out.println("Hello my friend! Let's introduce to each other!");
+        System.out.print("What is your name?:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Nice to meet you "+name);
+        System.out.print("How old are you, "+name+"?");
+        int age =scanner.nextInt();
+        String WhoOlder = age>myAge? "Oh "+name+", looks you are older than me": "Hey!  "+name+", you're younger than me! Good!!!";
+        System.out.println(WhoOlder);
 
     }
 }
