528410af10cbe55e4b87c8b7ec0191d4b75b7a67
daria-fsl <daria@fullstacklabs.co>
2021-04-28 16:57:35 -0400

    Homework 8 by Daria 04/28

---
+++ src/main/java/hw8daria/Book.java
@@ -0,0 +1,14 @@
+package hw8daria;
+
+public class Book {
+    public String name;
+    public String Author;
+    public String language;
+    public int pagenumbers;
+    public int rating;
+
+    public void Descr() {
+        System.out.println("Book name is " + this.name + " " + this.Author + " ");
+    }
+}
+
--- src/main/java/hw8daria/Clss.java
+++ src/main/java/hw8daria/Clss.java
@@ -1,5 +1,19 @@
 package hw8daria;
 
 public class Clss {
+    public static void main (String[]args){
+        Book coding = new Book ();
+        coding.name="Learn Java";
+        coding.Author="Shildt";
+        coding.language="English";
+        coding.pagenumbers=345;
+        coding.rating=5;
+
+        System.out.println(coding.name+" and the rating is "+coding.rating);
+
+        coding.Descr();
+    }
+
+
 
 }
