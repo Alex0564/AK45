38ebdef1c4fa94da00a119d8397f60a9d95af73d
Diana <kandian1@hotmail.com>
2021-04-28 16:44:04 -0400

    Homework5

---
+++ src/main/java/dianalesson8/Person.java
@@ -0,0 +1,7 @@
+package dianalesson8;
+
+public class Person {
+    public String name;
+    public String lastName;
+    public int year;
+}
---
+++ src/main/java/dianalesson8/Vehicle.java
@@ -0,0 +1,9 @@
+package dianalesson8;
+
+public class Vehicle {
+    public int year;
+    public String make;
+    public String model;
+    public String color;
+
+}
