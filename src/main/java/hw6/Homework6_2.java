5f4aef8d544cd6654387ef781f3aa5a7ee22c19b
HilolaT <khilola.tursun@gmail.com>
2021-04-28 12:40:23 -0500

    Homework for Lesson 6 Functions 2 Problem 2 Sorting array done

---
+++ src/main/java/hw6/Homework6_2.java
@@ -0,0 +1,17 @@
+package hw6;
+
+import java.util.Arrays;
+
+public class Homework6_2 {
+    public static void main(String[] args) {
+
+        // Homework for Lesson 6 Functions 2 Problem 3 Sorting integers in array
+
+        int[] arr2 = {3, 9, 1, 8, 4, 8, 5};
+        Arrays.sort(arr2);
+        System.out.println(Arrays.toString(arr2));
+
+    }
+    public static void sort(int[] arr2, int from_Index, int to_Index) {
+    }
+}
