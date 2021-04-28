b00c6b4462c6e655aa5c0567396036e5b9b2701f
HilolaT <khilola.tursun@gmail.com>
2021-04-28 00:05:14 -0500

    Homework to Lesson 1 Introduction to Java done

---
+++ src/main/java/hw1/Homework1.java
@@ -0,0 +1,27 @@
+package hw1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        // Homework for Lesson 1 Introduction to Java
+
+        System.out.println("Hello, my name is Hilola!");
+
+        System.out.println("Hello, my last name is Tursun!");
+
+        String a = "Hello, my name is Hilola!";
+        System.out.println(a);
+
+        String b = "Hello, my last name is Tursun!";
+        System.out.println(b);
+
+        String c = "My favorite numbers are: ";
+        int n1 = 44;
+        int n2 = 55;
+        System.out.println(c + n1 + " and " + n2);
+
+
+    }
+
+}
