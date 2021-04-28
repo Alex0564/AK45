96e07c66d10e50708fdfbc4328ab68f037171d96
daria-fsl <daria@fullstacklabs.co>
2021-04-28 17:24:16 -0400

    Homework 8 by Daria 04/28

--- src/main/java/hw8daria/Clss.java
+++ src/main/java/hw8daria/Clss.java
@@ -9,15 +9,28 @@ public class Clss {
         coding.pagenumbers=345;
         coding.rating=5;
 
+        Book cooking = new Book();
+        cooking.name="Tasty Life";
+        cooking.rating=4;
+        cooking.pagenumbers=567;
+        cooking.Author="Mary James";
+
         System.out.println(coding.name+" and the rating is "+coding.rating);
         coding.Descr();
+        cooking.Descr();
 
         Parfume baccarat = new Parfume ();
         baccarat.Name="Baccarat Rouge 540";
         baccarat.topnotes="wood and musk";
         baccarat.price=540;
 
+        Parfume cassiopea = new Parfume();
+        cassiopea.Name= "Cassiopea";
+        cassiopea.topnotes="sweet amber and vanilla";
+        cassiopea.price=400;
+
         baccarat.Features();
+        cassiopea.Features();
 
         Country Ukraine = new Country();
         Ukraine.name="Ukraine";
@@ -25,6 +38,17 @@ public class Clss {
         Ukraine.continent="Europe";
 
         Ukraine.D();
+
+        Phone iphone = new Phone();
+        iphone.make="Apple";
+        iphone.model="iPhone11";
+        iphone.color="white";
+        iphone.year=2021;
+        iphone.price=1200;
+
+        iphone.K();
+
+
 
     }
 
---
+++ src/main/java/hw8daria/Phone.java
@@ -0,0 +1,13 @@
+package hw8daria;
+
+public class Phone {
+    public String make;
+    public String model;
+    public int year;
+    public int price;
+    public String color;
+
+    public void K(){
+        System.out.println ("The company is "+this.make+" and the model is "+this.model+", the year is "+this.year+". The color is "+this.color+".");
+    }
+}
