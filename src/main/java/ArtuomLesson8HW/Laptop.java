2e7d62ce6ae109a55ca766bb257c4953797cc9cd
Academic202124 <academic198405@gmail.com>
2021-04-28 12:46:48 -0400

    Artuom Kudryashov-8 (Class, Method)

---
+++ src/main/java/ArtuomLesson8HW/Laptop.java
@@ -0,0 +1,18 @@
+package ArtuomLesson8HW;
+
+public class Laptop {
+    public String Laptopmodel;
+    public String Processormodel;
+    public String Operationsystem;
+    public String Browsername;
+
+    public  void  Help() {
+        System.out.println(Laptopmodel + " " + Processormodel + " " + Operationsystem + " Let me opportunity to install modern programs!");
+
+    }
+     public void HelpformyWife(){
+        String a="She like his color";
+        System.out.println("My wife like her laptop because "+a+" "+Processormodel+" "+Operationsystem+" "+" All of this give  opportunity to do her work!");
+
+    }
+}
---
+++ src/main/java/Lesson8ObjOrProg/Venicle.java
@@ -0,0 +1,17 @@
+package Lesson8ObjOrProg;
+
+public class Venicle {
+    public int year;
+    public String make;
+    public String model;
+    public  String color;
+
+    public void start(){
+        System.out.println(make+"  "+model+" started engine");
+    }
+    public void beep(){
+        System.out.println(color+ " car made beep");
+    }
+
+
+}
