db661c9b84a87ea0043baec15be8d69046b79a52
HilolaT <khilola.tursun@gmail.com>
2021-04-28 11:07:25 -0500

    Homework for Lesson 5 Functions 1 done

---
+++ src/main/java/hw5/Homework5.java
@@ -0,0 +1,47 @@
+package hw5;
+
+public class Homework5 {
+
+    public static void main(String[] args) {
+
+        // Homework for Lesson 5 Functions 1
+
+        printSumOfTwo(12, 34);
+
+        int xx = sumOfTwo(23, 46);
+        System.out.println("The sum of two numbers is " + xx);
+        System.out.println("**********************************");
+
+        System.out.println(isEven(2));                  // version 1 input
+        System.out.println("**********************************");
+
+        System.out.println(is_even(7));                 // version 2 input
+        System.out.println("**********************************");
+    }
+
+    // sum of two numbers:
+    public static void printSumOfTwo(int a, int b) {
+        System.out.println("The sum of two numbers is " + (a + b));
+        System.out.println("**********************************");
+    }
+
+    // sum of two numbers:
+    public static int sumOfTwo(int a1, int a2) {
+        int sum = a1 + a2;
+        return sum;
+    }
+
+    // version 1 for function isEven:
+    public static String isEven(int x) {
+        if (x % 2 == 0) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+
+    // version 2 for function is_even:
+    public static boolean is_even(int x) {
+        return x % 2 == 0;
+    }
+}
