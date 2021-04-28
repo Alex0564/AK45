9190fa0788b132348c6e2d11b761cdf02c437912
Timur Nessipbayev <nessi.tima@gmail.com>
2021-04-28 09:24:54 -0400

    Lesson8 Team

---
+++ src/main/java/HW8/Team.java
@@ -0,0 +1,10 @@
+package HW8;
+
+public class Team {
+    public String name;
+    public String Country;
+    public String City;
+    public int Founded;
+
+
+}
--- src/main/java/HW8/Work.java
+++ src/main/java/HW8/Work.java
@@ -7,7 +7,20 @@ public class Work {
         myFavPlayer.team= "Real Madrid";
         myFavPlayer.nationality="France";
         myFavPlayer.number = 5;
-        System.out.println("Player Name= "+myFavPlayer.name+ "\nTeam= " +myFavPlayer.team+"\nNationality= "+myFavPlayer.nationality+"\nShirt #= "+myFavPlayer.number);
 
+        System.out.println("Player Name: "+myFavPlayer.name+ "\nTeam: " +myFavPlayer.team+"\nNationality: "+myFavPlayer.nationality+"\nShirt #: "+myFavPlayer.number);
+        System.out.println("\n");
+
+
+
+
+
+        Team myTeam = new Team();
+        myTeam.name = "Arsenal";
+        myTeam.Country = "England";
+        myTeam.City = "London";
+        myTeam.Founded = 1886;
+
+        System.out.println("Team name: "+myTeam.name+"\nTeam Country: "+myTeam.Country+"\nTeam City: "+myTeam.City+"\nTeam Founded: "+myTeam.Founded);
     }
 }
