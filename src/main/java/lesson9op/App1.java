c95837b4ac8a5e17a711df6b030ada89a6d28fdd
OLGA <olgaprice77@gmail.com>
2021-04-29 18:47:47 -0400

    Lesson9op

--- src/main/java/lesson9op/App1.java
+++ src/main/java/lesson9op/App1.java
@@ -6,6 +6,13 @@ public class App1 {
         Student michaei = new Student("Michael", "Jobes",2000);
         Student samuel = new Student();
 
+        Fruit apple = new Fruit("Apple");
+        Fruit peach = new Fruit();
+        peach.setName("Peach");
+
+        System.out.println(apple.getName());
+        System.out.println(peach.getName());
+
 
     }
 
---
+++ src/main/java/lesson9op/Fruit.java
@@ -0,0 +1,16 @@
+package lesson9op;
+
+public class Fruit {
+    private  String name;
+    public Fruit(String name){
+        this.name = name;
+    }
+    public Fruit(){}
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
