7dc979510c545ff505635d74b5e4d0a208abef0b
otrdnv <otrdnvsmb@gmail.com>
2021-04-28 19:03:37 -0700

    HW9 started

---
+++ src/main/java/hw9/App.java
@@ -0,0 +1,19 @@
+package hw9;
+//        1. Create 4 classes with fields (at least 3 fields) and methods (at least 1 method)
+//        each class has at least 2 constructors (default and full)
+//        create printClass()/ printInfo() method to print out all fields
+//        fields are private
+//        each field has getter and setter
+//        2. In the App class create main() and create 2 instances of each class;
+//        one instance created used default constructor - fill up fields using setter
+//        second object created using full constructor
+//        print object info using print-out method
+//        3. Create Enum, Create class. use Enum
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+    }
+}
