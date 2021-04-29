5facf0d6d1cb4b197ca32a4751a7425cd9cdfbaf
aselb <asel_berdieva@yahoo.com>
2021-04-29 16:09:33 -0700

    homework1

---
+++ src/main/java/hw1/App.java
@@ -0,0 +1,16 @@
+package hw1;
+
+public class App {
+    public static void main(String[]args){
+        System.out.println(" Hello My name is Altynbek! ");
+        System.out.println(" Hello my lastname is Kopokov! ");
+        String name = "Altynbek";
+        System.out.println("Hello My name is " +name);
+        String lastname = "Kopokov";
+        System.out.println(" Hello My lastname is "+lastname);
+        int num1 = 44;
+        int num2 = 55;
+        System.out.println(" My favorite numbers are " +num1+  " and "  +num2);
+
+    }
+}
