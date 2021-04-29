0f6da3d70271ecf8c7e5074a3001bcdf5946cca0
TM84 <4513721@gmail.com>
2021-04-29 15:25:45 -0500

    lesson9_HomeWork_TM

---
+++ src/main/java/HWTM84_Less9/Drivers.java
@@ -0,0 +1,19 @@
+package HWTM84_Less9;
+
+public class Drivers {
+    private String name;
+    private String lastName;
+    private String id;
+    private String phone;
+
+    public void setDrivers(String name, String lastName, String id, String phone){
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.phone = phone;
+    }
+    public void printInfo(){
+        System.out.println("Name= "+name+", Lastname= "+lastName+", ID= "+id+", Phone= "+phone);
+    }
+}
+
