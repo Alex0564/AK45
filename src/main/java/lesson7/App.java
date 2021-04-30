238144c0e290746d0ff37a47c0e2e15ffa8fbcb5
QA25 <asyaboro25@gmail.com>
2021-04-30 15:59:45 -0400

    Home Work Lesson9 from QA25, Asya Boro

--- src/main/java/lesson7/App.java
+++ src/main/java/lesson7/App.java
@@ -2,6 +2,6 @@ package lesson7;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Lesson 7");
+        System.out.println("Hi");
     }
 }
---
+++ src/main/java/lesson8AB/Person.java
@@ -0,0 +1,10 @@
+package lesson8AB;
+
+public class Person {
+    public String name;
+    public String lastName;
+    public int year;
+    public void printInfo(){
+       System.out.println("Name= "+this.name+"\nLast NAme="+lastName+"\nYear");
+    }
+}
