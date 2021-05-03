483d4f24f212b57e7794649a4bbb041b66f3d80a
daria-fsl <daria@fullstacklabs.co>
2021-05-02 23:04:31 -0400

    Homework 9 by Daria 04/29

---
+++ src/main/java/hw9daria/Chocolate.java
@@ -0,0 +1,20 @@
+package hw9daria;
+
+public class Chocolate {
+    private String type;
+    private String taste;
+    private String brand;
+    private int weight;
+
+    public void setType(String type){ this.type=type; }
+    public String getType( ) {return type; }
+
+    public void setTaste(String taste) {this.taste = taste; }
+    public String getTaste(){return taste; }
+
+    public void setBrand(String brand) {this.brand=brand ;}
+    public String getBrand(){ return brand;}
+
+    public void setWeight(int weight) {this.weight=weight ;}
+    public int getWeight(){ return weight;}
+}
--- src/main/java/hw9daria/EncTwo.java
+++ src/main/java/hw9daria/EncTwo.java
@@ -7,8 +7,18 @@ public class EncTwo {
         rose.setColor("Red");
         rose.setPopularity("very popular");
         rose.setSteam("Long");
-        System.out.println(rose.getColor()+" is the color. "+rose.getName()+" is the name. Populariy: "+rose.getPopularity()+". And the steam is "+rose.getSteam()+".");
+        System.out.println(rose.getColor()+" is the color. "+rose.getName()+
+                " is the name. Populariy: "+rose.getPopularity()+". And the steam is "
+                +rose.getSteam()+".");
 
+        Chocolate Lindt = new Chocolate();
+        Lindt.setBrand("Lindt");
+        Lindt.setTaste("Bitter-sweet");
+        Lindt.setType("Dark");
+        Lindt.setWeight(100);
+        System.out.println(Lindt.getBrand()+" is the brand. "+Lindt.getTaste()+
+                " is the taste. The type is "+Lindt.getType()+". And the weight is "
+                +Lindt.getWeight()+" gramms.");
 
     }
 }
