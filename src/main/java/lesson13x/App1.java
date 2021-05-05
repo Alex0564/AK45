43ea91387a4196bfca3ed3b5940320c13c6d2310
Shamik <regarit@gmail.com>
2021-05-05 16:23:03 -0400

    Lesson 13x( Wrapper classes, references) - Finished

---
+++ src/main/java/lesson13x/App1.java
@@ -0,0 +1,26 @@
+package lesson13x;
+
+import homework12x.Parallelepiped;
+
+public class App1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = a;
+
+        a=10;
+        System.out.println(b);
+
+        Person p1 = new Person("Anna");
+        Person p2 = p1;
+
+        p1.setName("Vika");
+        System.out.println(p2.getName());
+
+        int[]arr = {1,2,3};
+        int[]brr = arr;
+        arr[0]=100;
+        System.out.println(brr[0]);
+
+
+    }
+}
