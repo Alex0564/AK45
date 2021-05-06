8cee1f67d5266d43a2ce7c9fa340f536564daaa8
QA25 <asyaboro25@gmail.com>
2021-05-06 00:55:45 -0400

    HomeWork12AB

--- src/main/java/homework12ab/App.java
+++ src/main/java/homework12ab/App.java
@@ -4,14 +4,24 @@ package homework12ab;
 
 public class App {
     public static void main(String[] args) {
-        IHumanBody body = new eukaryoticCell("cytoplasm", 206);
-            IHumanCell cell=new prokaryoticCell();
-
-
+        IHumanBody body = new IHumanBody() {
+            @Override
+            public void muscles() {
+                System.out.println("There are about 600 muscles in the human body.");
 
             }
 
-        }
+            @Override
+            public int bones() {
+                return 206;
+            }
+
+           IBacteriaCell cell = (IBacteriaCell) new prokaryoticCell();
+
+        };
+    }
+}
+
 
 
 
