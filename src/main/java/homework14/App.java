b96544c8a6ed63bc7ec04b9abbe31b761661d285
ekaterina terekhovich <terehovich@gmail.com>
2021-05-05 22:50:09 -0700

    MapHashMapSet

---
+++ src/main/java/homework14/App.java
@@ -0,0 +1,97 @@
+package homework14;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+
+//        Create 3 HashMap- <String,String>, <Integer,String> and custom classes <Class1,Class2>
+//                add 4 elements to each hashmap
+//        try methods  put/replace/remove/get/foreach
+
+        Map<String, String> bestBeaches = new HashMap<>();
+        bestBeaches.put("Navagio", "Greece");
+        bestBeaches.put("Malibu", "USA");
+        bestBeaches.put("Zuma", "USA");
+        bestBeaches.put("Bondi", "Australia");
+        bestBeaches.put("Nissi", "Fiji");
+//        System.out.println(bestBeaches);
+//        System.out.println(bestBeaches.get("Zuma"));
+        bestBeaches.replace("Nissi", "Cyprus");
+
+        for (String key : bestBeaches.keySet()) {
+            System.out.println(key);
+        }
+
+//        ..........new HashMap...........
+
+        Map<Integer, String> countries = new HashMap<>();
+        countries.put(1, "China");
+        countries.put(2, "India");
+        countries.put(3, "USA");
+        countries.put(4, "Indonesia");
+
+        for (String value : countries.values()) {
+            System.out.println(value);
+        }
+
+//          .......new HashMap.......
+
+        Map<Student, Subject> major = new HashMap<>();
+
+        Student kate = new Student("Kate");
+        Student tamara = new Student("Tamara");
+        Student jack = new Student("Jack");
+        Student oleg = new Student("Oleg");
+
+        Subject math = new Subject("Math");
+        Subject history = new Subject("History");
+        Subject chemistry = new Subject("Chemistry");
+        Subject philosophy = new Subject("Philosophy");
+
+        major.put(kate, math);
+        major.put(tamara, history);
+        major.put(jack, chemistry);
+        major.put(oleg, philosophy);
+
+        for (Subject value : major.values()) {
+            System.out.println(value.toString(major.values()));
+        }
+        for (Student key : major.keySet()) {
+            System.out.println(key.toString(major.keySet()));
+        }
+
+//        Create 2 HashSet- <String>,  and custom classes <Class1>
+//        add 4 elements to each hashset
+
+        Set<String> beaches = new HashSet<>();
+        beaches.add("Santa Monica");
+        beaches.add("Malibu");
+        beaches.add("Navagio");
+        beaches.add("Bondi");
+//        System.out.println(beaches);
+        beaches.remove("Navagio");
+        beaches.add("Ora");
+        beaches.remove("Malibu");
+
+        for (String beachesAll : beaches) {
+            System.out.println(beachesAll);
+        }
+
+        System.out.println(beaches.contains("Ora"));
+        System.out.println(beaches.contains("Zuma"));
+        beaches.add("Zuma");
+        System.out.println(beaches);
+
+        Set<Student> students = new HashSet<>();
+        students.add(kate);
+        students.add(tamara);
+        students.add(jack);
+        students.add(oleg);
+
+//        не смогла распечатать!!!
+    }
+}
---
+++ src/main/java/homework14/Subject.java
@@ -0,0 +1,23 @@
+package homework14;
+
+import java.util.Collection;
+
+public class Subject {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Subject(String name) {
+        this.name = name;
+    }
+
+    public Object toString(Object String){
+    return name;
+    }
+}
