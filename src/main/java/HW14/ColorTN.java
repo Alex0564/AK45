dd5276226baa666ff5fbfab6f77cab4aad84f8ff
Timur Nessipbayev <nessi.tima@gmail.com>
2021-05-07 15:13:04 -0400

    HW14

---
+++ src/main/java/HW14/ColorTN.java
@@ -0,0 +1,13 @@
+package HW14;
+
+public class ColorTN {
+    private String color;
+
+    public ColorTN(String color) {
+        this.color = color;
+    }
+
+    public String getCountries() {
+        return color;
+    }
+}
---
+++ src/main/java/HW14/FruitTN.java
@@ -0,0 +1,13 @@
+package HW14;
+
+public class FruitTN {
+    private String name;
+
+    public FruitTN(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
