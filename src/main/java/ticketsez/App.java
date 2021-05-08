d6b7cfb0711ff437f863fb3d87cbdce706f6dc4f
Zhangulov <eldarkeywest@gmail.com>
2021-05-07 21:23:38 -0400

    Lesson 1 - Unit tests

---
+++ src/main/java/ticketsez/App.java
@@ -0,0 +1,8 @@
+package ticketsez;
+
+public class App {
+    public static void main(String[] args) {
+        Tickets passanger =new Tickets(10,1000);
+        System.out.println(passanger.getPrice());
+    }
+}
