da896f64a471072a457d362e867f39d16b3cbe8d
oleks <oleksiy564@gmail.com>
2021-05-09 00:02:40 -0400

    home work lesson 15 the last part May 9 2021

--- src/main/java/less15homework/App.java
+++ src/main/java/less15homework/App.java
@@ -1,7 +1,12 @@
 package less15homework;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
-
+        Map<Integer, String > rooms = new HashMap<>();
+//        List<>;
     }
 }
