a1b4698525041c319f1467e2a4c251454d3085c8
HilolaT <khilola.tursun@gmail.com>
2021-05-10 01:09:26 -0500

    Updates

--- src/main/java/extraLesson/Student.java
+++ src/main/java/extraLesson/Student.java
@@ -1,9 +1,14 @@
 package extraLesson;
 
 public class Student extends Person{
-    private final String major;
+    private String major;
     public Student(String name, String lastName, int year, String major) {
         super(name, lastName, year);
         this.major=major;
     }
+//    @Override
+//    public void hello(){
+//        System.out.println("HI");
+//    }
+
 }
--- src/main/java/lesson10HT/App.java
+++
@@ -1,7 +0,0 @@
-package lesson10HT;
-
-public class App {
-    public static void main(String[] args) {
-
-    }
-}
