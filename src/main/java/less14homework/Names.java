ad2d50021749038408ad45b4776e0eaa82ff3617
oleks <oleksiy564@gmail.com>
2021-05-10 20:05:57 -0400

    home work lesson 14 the last part May 10 2021

---
+++ src/main/java/less14homework/Names.java
@@ -0,0 +1,18 @@
+package less14homework;
+
+public class Names {
+   private String name;
+
+
+
+   @Override
+   public String toString() {return " Name: " + name;}
+
+   public Names(String name) {this.name = name;}
+   public String getName() {return name;}
+   public void setName(String name) {this.name = name;}
+
+   public Names() {
+   }
+
+}
---
+++ src/main/java/less14homework/Surnames.java
@@ -0,0 +1,15 @@
+package less14homework;
+
+public class Surnames {
+    public Surnames() {
+    }
+
+    @Override
+    public String toString() {return "\n lastname:  " + lastname;}
+
+    private String lastname;
+    public Surnames(String lastname) {this.lastname = lastname;}
+    public String getLastname() {return lastname;}
+    public void setLastname(String lastname) {this.lastname = lastname;}
+
+}
