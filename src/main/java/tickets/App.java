aa64283982b3006eb7b2bef6ae20df193fedf062
Val Kan <valeriy.kan@gmail.com>
2021-05-10 22:06:42 -0400

    lesson2 css selectors

--- src/main/java/tickets/App.java
+++ src/main/java/tickets/App.java
@@ -2,7 +2,7 @@ package tickets;
 
 public class App {
     public static void main(String[] args) {
-        Tickets passenger = new Tickets(75,1000);
+        Tickets passenger = new Tickets(2,1000);
         System.out.println(passenger.getPrice());
     }
 }
