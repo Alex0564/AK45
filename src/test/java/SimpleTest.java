e7b272fc9c95258aad7131eef085005c5e4254a9
Eugene Apashniuk <asting2006@rambler.ru>
2021-05-12 11:42:38 -0400

    Selenium. Upload chromedriver

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void udemySearch() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://udemy.com");
+
+        Thread.sleep(5000);
+        driver.quit();
+
+
+
+    }
+}
