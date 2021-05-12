20fc1ba3b1166f91b0e2c8a5900439b36a3232d8
oleks <oleksiy564@gmail.com>
2021-05-12 19:15:16 -0400

    homework 13 May 12 2021

--- src/main/java/less15homework/App.java
+++ src/main/java/less15homework/App.java
@@ -8,32 +8,45 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
 //        List og rooms
-//        Map<Integer, String > rooms = new HashMap<>();
-//        List<InsuranceCompanies> acceptInsurances = new ArrayList<>() ;
-//        rooms.put(101, "Anesthesia");
-//        rooms.put(102, "Medicine");
-//        rooms.put(103, "Dermatology");
-//        rooms.put(104, "Surgery");
-//        rooms.put(106, "Pharmacy");
-//        rooms.put(107, "Otolaryngology");
-//        rooms.put(108, "Neurology");
-//        rooms.put(109, "Surgery");
-//        rooms.put(110, "Urology");
+        Map<Integer, String > rooms = new HashMap<>();
+        List<InsuranceCompanies> acceptInsurances = new ArrayList<>() ;
+        List<Doctor> doctors = new ArrayList<>();
+        rooms.put(101, "Anesthesia");
+        rooms.put(102, "Medicine");
+        rooms.put(103, "Dermatology");
+        rooms.put(104, "Surgery");
+        rooms.put(106, "Pharmacy");
+        rooms.put(107, "Otolaryngology");
+        rooms.put(108, "Neurology");
+        rooms.put(109, "Urology");
 //        System.out.println(rooms);
 //
 ////        List of Insurance Companies
-//        acceptInsurances.add(InsuranceCompanies.ANTHEM);
-//        acceptInsurances.add(InsuranceCompanies.HCSC);
-//        acceptInsurances.add(InsuranceCompanies.HHSC);
-//        acceptInsurances.add(InsuranceCompanies.OSCAR);
-//        acceptInsurances.add(InsuranceCompanies.FIDELIS);
-//        System.out.println("Insurance Companies: " + acceptInsurances);
-//
-////     List og Hospitals
-//        Address addressOfSinay = new Address("4573 7th Ave", "Phoenox", "AZ", 85041 );
-//        Address addressOfMayo = new Address();
-//        Address addressOfUCLA = new Address();
-//        Address addressOfUCFS = new Address();
+        acceptInsurances.add(InsuranceCompanies.ANTHEM);
+        acceptInsurances.add(InsuranceCompanies.HCSC);
+        acceptInsurances.add(InsuranceCompanies.HHSC);
+        acceptInsurances.add(InsuranceCompanies.OSCAR);
+        acceptInsurances.add(InsuranceCompanies.FIDELIS);
+//      System.out.println("Insurance Companies: " + acceptInsurances);
+
+//      List of Address of Hospitals
+        Address addressOfNYULH = new Address("150 55th Str", "Brooklyn", "NY", 11220 ); // NYU Langone Hospitals, New York City
+        Address addressOfMayo = new Address("200 1st NW", "Rochester", "MN", 55901); // Mayo Clinic
+        Address addressOfJHHB = new Address("1800 Orleans Street", "Baltimore", "MD", 21287); // Johns Hopkins Hospital, Baltimore
+        Address addressOfMGHB = new Address("55 Fruit Str", "Boston", "MA", 02114); // Massachusetts General Hospital, Boston
+        Address addressOfNMHC = new Address("251 E Huron St", "Chicago", "IL", 606116); // Northwestern Memorial Hospital, Chicago
+
+//        List of Doctors
+        Doctor maher = new Doctor ("Philip", "Maher", Position.DERMATOLOGIST);
+        Doctor mitchel = new Doctor("Mitchel", "Dale", Position.NEUROLOGIST);
+        Doctor ferrara = new Doctor("Ferrara", "Anthony", Position.OTOLARYNGOLOGIST);
+        Doctor singh = new Doctor("Singh", "Joshua", Position.SURGEON);
+        Doctor duran = new Doctor("Duran", "Hugo", Position.UROLOGIST);
+
+//        List of Hospitals
+//        Hospital nyulh = new Hospital("NYU Langone Hospitals", addressOfNYULH, doctors.add()  , 103, InsuranceCompanies.ANTHEM );
+
+
 
     }
 }
