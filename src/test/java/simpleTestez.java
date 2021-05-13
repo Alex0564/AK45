152fe8d8a8f579c2799badd6387a00a4197b6644
Zhangulov <eldarkeywest@gmail.com>
2021-05-12 20:17:29 -0400

    Lesson 3- xPath

--- src/test/java/simpleTestez.java
+++ src/test/java/simpleTestez.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class simpleTestez {
@@ -7,8 +11,19 @@ public class simpleTestez {
     public void udemySearch() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://udemy.com");
+
+        WebElement searchField = driver.findElement(By.xpath("(//*[@name='q'])[1]"));
+        searchField.sendKeys("Java");
+        Thread.sleep(2000);
+        searchField.sendKeys(Keys.ENTER);
+
+        Thread.sleep(2000);
+        WebElement result = driver.findElement(By.tagName("h1"));
+
+        Assert.assertEquals("10,000 results for “java”",result.getText());
+
+
 
         Thread.sleep(5000);
         driver.quit();
