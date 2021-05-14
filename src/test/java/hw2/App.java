f7c483a397fd1597f369dbb6c7c576158790daf8
otrdnv <otrdnvsmb@gmail.com>
2021-05-13 19:18:02 -0700

    HW17 done

---
+++ src/test/java/hw2/App.java
@@ -0,0 +1,9 @@
+package hw2;
+
+public class App {
+//    //*[@src ="/staticx/udemy/images/v6/logo-coral.svg"]/parent::*/following-sibling::div[2]
+//    (//*[@aria-label = "Submit search"])[2]//parent::*/parent::*/parent::*/parent::*/h1
+//    (//*[text()="Python"])[1]/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/parent::*/div[2]/div
+//    (//*[@href="/topic/python/"])[1]/parent::*/parent::*/parent::*/parent::*/parent::*/div/div/div/div[7]
+
+}
