dc8f9ae7a87048351f0be83d0d7623924c26618f
baur <baurzhan.zh@gmail.com>
2021-05-13 21:22:24 -0400

    Lesson 5 - TestNG annotations

--- src/test/java/simpleTest/KoelLogin.java
+++ src/test/java/simpleTest/KoelLogin.java
@@ -1,19 +1,32 @@
 package simpleTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class KoelLogin {
-    @Test
-    public void loginToKoel_correctCredentials() throws InterruptedException {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void starUp() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://bbb.testpro.io/");
         Thread.sleep(1000);
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+    @Test
+    public void loginToKoel_correctCredentials() throws InterruptedException {
         WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
         WebElement password = driver.findElement(By.cssSelector("[type='password']"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
@@ -26,15 +39,9 @@ public class KoelLogin {
 
         WebElement home = driver.findElement(By.cssSelector(".home")); //*[@class = 'home active']
         Assert.assertTrue(home.isDisplayed());
-        Thread.sleep(3000);
-        driver.quit();
     }
     @Test
     public void loginToKoel_incorrectCredentials() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://bbb.testpro.io/");
-        Thread.sleep(1000);
         WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
         WebElement password = driver.findElement(By.cssSelector("[type='password']"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
@@ -47,7 +54,28 @@ public class KoelLogin {
 
         WebElement home = driver.findElement(By.cssSelector(".error")); //*[@class = 'home active']
         Assert.assertTrue(home.isDisplayed());
-        Thread.sleep(3000);
-        driver.quit();
+    }
+    @Test
+    public void loginToKoel_createPlaylist() throws InterruptedException {
+        WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
+        WebElement password = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        email.sendKeys("koeluser06@testpro.io");
+        password.sendKeys("te$t$tudent");
+        loginButton.click();
+
+        Thread.sleep(2000);
+        WebElement plusButton = driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']"));
+        plusButton.click();
+        WebElement newPlaylist = driver.findElement(By.xpath("//*[text()='New Playlist']"));
+        newPlaylist.click();
+        WebElement textField = driver.findElement(By.xpath("//*[@class='create']/input"));
+        textField.sendKeys("XXXX");
+        textField.sendKeys(Keys.RETURN);
+        Thread.sleep(200);
+        WebElement green = driver.findElement(By.xpath("//*[@class='success show']"));
+        Assert.assertTrue(green.isDisplayed());
+
     }
 }
