cbb6ed8c156b5c3ce03414b2b65d48169f774449
oleks <oleksiy564@gmail.com>
2021-05-14 20:00:56 -0400

    Coursera 15 May 14 2021

--- src/test/java/simpleTest/CourseraLogin.java
+++ src/test/java/simpleTest/CourseraLogin.java
@@ -4,15 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CourseraLogin {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void startUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.coursera.org/");
+        Thread.sleep(3000);}
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
     @Test
     public void loginToCoursera_correctCredentials()throws InterruptedException{
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.coursera.org/");
-        Thread.sleep(3000);
+
         WebElement exit = driver.findElement(By.xpath("//*[text()='Войти']"));
         exit.click();
         Thread.sleep(3000);
@@ -24,8 +38,6 @@ public class CourseraLogin {
         password.sendKeys("intell10");
         Thread.sleep(3000);
         loginButton.click();
-        Thread.sleep(3000);
 
-        driver.quit();
     }
 }
