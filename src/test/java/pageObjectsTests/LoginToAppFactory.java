9f188ceae7c2f32f9476f04c68fdf68bd1b103be
baur <baurzhan.zh@gmail.com>
2021-05-21 21:26:03 -0400

    Lesson 11 - TestNG

--- src/test/java/pageObjectsTests/LoginToAppFactory.java
+++ src/test/java/pageObjectsTests/LoginToAppFactory.java
@@ -7,7 +7,7 @@ import pageObjects.LoginPageFactory;
 import pageObjects.MainPage;
 
 public class LoginToAppFactory extends BaseTest {
-    @Test
+    @Test(enabled = false)
     public void loginToApp_correctCredentials_successfulLogin(){
         LoginPageFactory loginPage = new LoginPageFactory(driver);
         loginPage.open("https://bbb.testpro.io");
@@ -16,6 +16,48 @@ public class LoginToAppFactory extends BaseTest {
     }
     @Test
     public void loginToApp_incorrectCredentials_failedLogin(){
+        LoginPageFactory loginPage = new LoginPageFactory(driver);
+        loginPage.open("https://bbb.testpro.io");
+        loginPage.loginToApp(username,"wrongPassword");
+        Assert.assertTrue(loginPage.isErrorFrame());
+    }
+    @Test(enabled = false)
+    public void loginToApp_correctCredentials_successfulLogin1(){
+        LoginPageFactory loginPage = new LoginPageFactory(driver);
+        loginPage.open("https://bbb.testpro.io");
+        MainPage mainPage = loginPage.loginToApp(username,password);
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void loginToApp_incorrectCredentials_failedLogin1(){
+        LoginPageFactory loginPage = new LoginPageFactory(driver);
+        loginPage.open("https://bbb.testpro.io");
+        loginPage.loginToApp(username,"wrongPassword");
+        Assert.assertTrue(loginPage.isErrorFrame());
+    }
+    @Test
+    public void loginToApp_correctCredentials_successfulLogin2(){
+        LoginPageFactory loginPage = new LoginPageFactory(driver);
+        loginPage.open("https://bbb.testpro.io");
+        MainPage mainPage = loginPage.loginToApp(username,password);
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void loginToApp_incorrectCredentials_failedLogin2(){
+        LoginPageFactory loginPage = new LoginPageFactory(driver);
+        loginPage.open("https://bbb.testpro.io");
+        loginPage.loginToApp(username,"wrongPassword");
+        Assert.assertTrue(loginPage.isErrorFrame());
+    }
+    @Test
+    public void loginToApp_correctCredentials_successfulLogin3(){
+        LoginPageFactory loginPage = new LoginPageFactory(driver);
+        loginPage.open("https://bbb.testpro.io");
+        MainPage mainPage = loginPage.loginToApp(username,password);
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void loginToApp_incorrectCredentials_failedLogin3(){
         LoginPageFactory loginPage = new LoginPageFactory(driver);
         loginPage.open("https://bbb.testpro.io");
         loginPage.loginToApp(username,"wrongPassword");
