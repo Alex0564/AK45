e271cf8a9c8dcd378a350cd5c34ff448584312ad
daria-fsl <daria@fullstacklabs.co>
2021-05-22 20:29:43 -0400

    Commit for Tests

--- src/test/java/pageObjectsTests/LoginToApp.java
+++ src/test/java/pageObjectsTests/LoginToApp.java
@@ -1,6 +1,10 @@
 package pageObjectsTests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pageObjects.LoginPage;
 import pageObjects.MainPage;
