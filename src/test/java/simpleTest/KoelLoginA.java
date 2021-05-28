527a28aaf1f59bca4e19ac38b66a2db74a752bb7
timur <timur895289@gmail.com>
2021-05-28 16:54:22 -0400

    lesson - 6A

--- src/test/java/simpleTest/KoelLoginA.java
+++ src/test/java/simpleTest/KoelLoginA.java
@@ -3,48 +3,69 @@ package simpleTest;
 import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class KoelLoginA {
 
-    WebDriver driver;
+   private WebDriver driver;
+   private Wait<WebDriver> wait;
+   private Wait<WebDriver> fluentWait;
     @BeforeMethod
-        public void starUp() throws InterruptedException {
+        public void starUp()  {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,5,200);
+        fluentWait = new FluentWait<>(driver)
+                .pollingEvery(Duration.ofMillis(200))
+                .withTimeout(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(ElementClickInterceptedException.class)
+                .ignoring(ElementNotInteractableException.class)
+                .ignoring(StaleElementReferenceException.class);
+
+
+
+
+
+   //     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://bbb.testpro.io/");
-        Thread.sleep(1000);
     }
     @AfterMethod
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(1000);
+    public void tearDown()  {
+
         driver.quit();
     }
 
     @Test
-    public void loginToKoel_correctCredentials() throws InterruptedException {
-
-        WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
+    public void loginToKoel_correctCredentials()  {
+        By emailBy = By.xpath("//*[@type='email']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(emailBy));
+        WebElement email = driver.findElement(emailBy);
         WebElement password = driver.findElement(By.xpath("//*[@type='password']"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
 
         email.sendKeys("timur895289@gmail.com");
         password.sendKeys("te$t$tudent");
         loginButton.click();
-        Thread.sleep(3000);
 
-        WebElement home = driver.findElement(By.cssSelector(".home"));
+        By homeBy = By.cssSelector(".home");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(homeBy));
+        WebElement home = driver.findElement(homeBy);
         Assert.assertTrue(home.isDisplayed());
 
     }
     @Test
-    public void loginToKoel_incorrectCredentials() throws InterruptedException {
-
+    public void loginToKoel_incorrectCredentials() {
+        By emailBy = By.xpath("//*[@type='email']");
+        fluentWait.until(x->x.findElement(emailBy).isDisplayed());
         WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
         WebElement password = driver.findElement(By.xpath("//*[@type='password']"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
@@ -52,31 +73,42 @@ public class KoelLoginA {
         email.sendKeys("timur895289@gmail.com");
         password.sendKeys("wrongPassword");
         loginButton.click();
-        Thread.sleep(500);
-        WebElement home = driver.findElement(By.cssSelector(".error"));
+
+        By errorBy = By.cssSelector(".error");
+        fluentWait.until(x->x.findElement(errorBy).isDisplayed());
+        WebElement home = driver.findElement(errorBy);
         Assert.assertTrue(home.isDisplayed());
     }
     @Test
-    public void loginToKoel_createPlaylist() throws InterruptedException {
-        WebElement email = driver.findElement(By.xpath("//*[@type='email']"));
+    public void loginToKoel_createPlaylist() {
+        By emailBy = By.xpath("//*[@type='email']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(emailBy));
+
+        WebElement email = driver.findElement(emailBy);
         WebElement password = driver.findElement(By.xpath("//*[@type='password']"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
-        Thread.sleep(1000);
+
 
         email.sendKeys("timur895289@gmail.com");
         password.sendKeys("te$t$tudent");
         loginButton.click();
-        Thread.sleep(3000);
 
-        WebElement plusButton = driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']"));
+       By plusButtonBy = By.xpath("//*[@class='fa fa-plus-circle control create']");
+       wait.until(ExpectedConditions.elementToBeClickable(plusButtonBy));
+
+        WebElement plusButton = driver.findElement(plusButtonBy);
         plusButton.click();
+
+
         WebElement newPlaylist = driver.findElement(By.xpath("//*[text()='New Playlist']"));
         newPlaylist.click();
-        WebElement textField = driver.findElement(By.xpath("//*[@required='required'][1]"));
+        WebElement textField = driver.findElement(By.xpath("//*[@class='create']/input"));
         textField.sendKeys("XXXX");
         textField.sendKeys(Keys.RETURN);
-        Thread.sleep(500);
-        WebElement green = driver.findElement(By.xpath("//*[@class='success show']"));
+        By successBy= By.xpath("//*[@class='success show']");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(successBy));
+
+        WebElement green = driver.findElement(successBy);
         Assert.assertTrue(green.isDisplayed());
 
 
