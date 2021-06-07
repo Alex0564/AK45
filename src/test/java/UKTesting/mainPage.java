d6f53817494aa3fbc1c9431d095ab40fedf28f6b
Umida99 <umida.kaxarova@bk.ru>
2021-06-06 21:03:09 -0400

    UKTesting

---
+++ src/test/java/UKTesting/mainPage.java
@@ -0,0 +1,14 @@
+package UKTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.BeforeMethod;
+
+public class mainPage {
+    private WebDriver driver;
+    private Wait<WebDriver>wait;
+
+
+
+
+}
